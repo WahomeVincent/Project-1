f07f8ebc2ced38454e2d2d607e1b11f3cbee24e5
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-12 15:28:58 +0300

    This is my js file

---
+++ index.js
@@ -0,0 +1,24 @@
+function showMoreLeopard() {
+    var text = document.getElementById("text")
+    text.innerHTML = "leopard, (Panthera pardus), , also called panther, large cat closely related to the lion, tiger, and jaguar. The name leopard was originally given to the cat now called cheetah—the so-called hunting leopard—which was once thought to be a cross between the lion and the pard. The term pard was eventually replaced by the name leopard."
+}
+
+function showMoreLion() {
+    var text = document.getElementById("text")
+    text.innerHTML = "The lion is a large cat of the genus Panthera native to Africa and India. It has a muscular, broad-chested body; short, rounded head; round ears; and a hairy tuft at the end of its tail. It is sexually dimorphic; adult male lions are larger than females and have a prominent mane"
+}
+
+function showMoreElephant() {
+    var text = document.getElementById("text")
+    text.innerHTML = "Elephants are the largest existing land animals. Three living species are currently recognised: the African bush elephant, the African forest elephant, and the Asian elephant. They are the only surviving members of the family Elephantidae and the order Proboscidea."
+}
+
+function showMoreBuffalo() {
+    var text = document.getElementById("text")
+    text.innerHTML = "The African buffalo is a large sub-Saharan African bovine. There are five subspecies that are recognized as being valid. Syncerus caffer caffer, the Cape buffalo, is the nominotypical subspecies, and the largest one, found in Southern and East Africa."
+}
+
+function showMoreRhino() {
+    var text = document.getElementById("text")
+    text.innerHTML = "A rhinoceros, commonly abbreviated to rhino, is a member of any of the five extant species of odd-toed ungulates in the family Rhinocerotidae. Two of the extant species are native to Africa, and three to South and Southeast Asia."
+}
